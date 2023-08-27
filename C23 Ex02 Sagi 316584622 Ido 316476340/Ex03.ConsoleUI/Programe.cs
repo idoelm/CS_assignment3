f240f18e983493c47c7524fea8e79f77d82630a9
idoelm <idoelm18@gmail.com>
2023-08-27 17:21:32 +0300

    x

---
+++ C23 Ex02 Sagi 316584622 Ido 316476340/Ex03.ConsoleUI/Programe.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+using Ex03.GarageLogic;
+namespace Ex03.ConsoleUI
+{
+    public class Programe
+    {
+        public static void Main()
+        {
+            Garage myGarage = new Garage();
+            myGarage.StartGarage();
+        }
+    }
+
+}
